f5d9172a24a0407d77c947ed9a1d5d8bb672ccf9
Eduard Beukes <edpbeukes@gmail.com>
2023-03-21 16:19:33 +0200

    Added some functionality.

--- UnityProject/Assets/Scripts/GameManager.cs
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -10,11 +11,13 @@ public class GameManager : MonoBehaviour
         // controls the game win/lose conditions.
         // Manages which room/creature/treasure spawns(eventually).
         
+        [SerializeField,Tooltip("The player in the game.")]
+        private Player player;
+        
         [SerializeField,Tooltip("The creature that the player is fighting.")]
         private Creature enemyCreature;
 
-        [SerializeField,Tooltip("The player in the game.")]
-        private Player player;
+        private int _roundNumber = 1;
         
         public void Awake()
         {
@@ -23,6 +26,10 @@ public class GameManager : MonoBehaviour
         
         public void ResetGame()
         {
-            
+            Debug.Log("Resetting the game.");
+
+            _roundNumber = 1;
+            player.GetComponent<HitPoints>()!.ResetHealth();
+            enemyCreature.GetComponent<HitPoints>()!.ResetHealth();
         }
 }
