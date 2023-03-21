cdf2ee633202f524c33f7c3b49f612f447a37a14
Eduard Beukes <edpbeukes@gmail.com>
2023-03-21 15:56:21 +0200

    Added reset functionality to the hitpoints script.

--- UnityProject/Assets/Scripts/HitPoints.cs
+++ UnityProject/Assets/Scripts/HitPoints.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class HitPoints : MonoBehaviour
 {
     [SerializeField,Tooltip("The maximum hit points possible.")]
-    private int maxHitPoints;
+    private int maxHitPoints = 100;
     
     [SerializeField, Tooltip("The current hit points available.")]
     private int currentHitPoints;
 
+    [SerializeField, Tooltip("Define whether health should reset on awake. ")]
+    private bool resetHealth = true;
+    public void Awake()
+    {
+        if (resetHealth)
+            currentHitPoints = maxHitPoints;
+    }
+    
     public int CurrentHitPoints
     {
         get { return currentHitPoints; }
