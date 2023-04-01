de9087d90645942b0c165747912f0ac3904776cb
Eduard Beukes <edpbeukes@gmail.com>
2023-04-01 04:58:40 +0200

    Added the base effect of all artifact effects.

---
+++ UnityProject/Assets/Scripts/ArtifactEffects/ArtifactEffect.cs
@@ -0,0 +1,12 @@
+
+namespace ArtifactEffects
+{
+    /// <summary>
+    /// The base effect that all artifact effects should inherit from.
+    /// </summary>
+    public abstract class ArtifactEffect
+    {
+        public abstract void Effect();
+
+    }
+}
