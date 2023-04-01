8cc1712884cdeec73bca283dcf1d581dfe9e7783
Eduard Beukes <edpbeukes@gmail.com>
2023-04-01 04:57:41 +0200

    Added architecture to idenfiy character attributes.

---
+++ UnityProject/Assets/Scripts/GameArchitecture/CharacterAttributeType.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace GameArchitecture
+{
+    /// <summary>
+    /// USed to identify the type of a Character Attribute
+    /// </summary>
+    [CreateAssetMenu(
+    menuName = "Game Architecture/Character Attribute Type",
+    fileName = "CharacterAttribute")]
+    public class CharacterAttributeType : ScriptableObject
+    {
+    }
+}
+
