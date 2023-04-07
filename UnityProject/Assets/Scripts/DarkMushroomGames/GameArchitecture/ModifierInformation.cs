89d59e7aef249e4c2311e1d6e5242db2b0454c3a
Eduard Beukes <edpbeukes@gmail.com>
2023-04-07 09:19:25 +0200

    Added the modifier information class to the project.

---
+++ UnityProject/Assets/Scripts/DarkMushroomGames/GameArchitecture/ModifierInformation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DarkMushroomGames.GameArchitecture
+{
+    [Serializable]
+    public class ModifierInformation
+    {
+        public ModifierType Type;
+        public int Value;
+    }
+}
