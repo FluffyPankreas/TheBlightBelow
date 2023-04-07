9d39e2e87db904dbf4fff49cfa7c72076ccd3fea
Eduard Beukes <edpbeukes@gmail.com>
2023-04-07 10:22:25 +0200

    Added some todos and debug code.

--- UnityProject/Assets/Scripts/DarkMushroomGames/GameArchitecture/Managers/CharacterManager.cs
+++ UnityProject/Assets/Scripts/DarkMushroomGames/GameArchitecture/Managers/CharacterManager.cs
@@ -1,4 +1,5 @@
 using DarkMushroomGames.Architecture;
+using UnityEngine;
 
 namespace DarkMushroomGames.GameArchitecture.Managers
 {
@@ -9,13 +10,17 @@ namespace DarkMushroomGames.GameArchitecture.Managers
     /// </summary>
     public class CharacterManager : MonoBehaviourSingleton<CharacterManager>
     {
+        //Todo: Add runtime variables for the character attributes here(eg. currentHealth, maxHealth)
+        //Todo: Add ways to initialize variables for when encounters start.
+        //Todo: Add ways to initialize variables for when runs start/load.
+        
         /// <summary>
         /// Handles events for when the modifier queue changes. It checks for relevant modifiers and applies
         /// effects appropriately. 
         /// </summary>
         public void OnModifierQueueChanged()
         {
-            
+            Debug.Log("OnModifierQueueChanged.");
         }
     }
 }
