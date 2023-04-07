4f931c770348ac127ca0205202250f8f123523dd
Eduard Beukes <edpbeukes@gmail.com>
2023-04-07 09:48:54 +0200

    Updated modifier queue to gracefully handle it when the event hasn't been assigned.

--- UnityProject/Assets/Scripts/DarkMushroomGames/GameArchitecture/ModifierQueue.cs
+++ UnityProject/Assets/Scripts/DarkMushroomGames/GameArchitecture/ModifierQueue.cs
@@ -1,17 +1,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DarkMushroomGames.Architecture;
+using UnityEngine.Serialization;
 
 namespace DarkMushroomGames.GameArchitecture
 {
     public class ModifierQueue : MonoBehaviourSingleton<ModifierQueue>
     {
-        [SerializeField] private List<ModifierInformation> modifierQueue = new();
+        private readonly List<ModifierInformation> _modifierQueue = new();
 
+        public CCGKit.GameEvent onEnqueueEvent;
+
+        public void Awake()
+        {
+            Debug.Assert(onEnqueueEvent != null,
+                "The onEnqueueEvent isn't assigned. This will prevent other systems from responding properly. Please make sure you are using a prefab for the singleton.",
+                gameObject);
+        }
+        
         public void AddToQueue(ModifierType type, int value )
         {
-            // Todo: Add the event raise here.
-            modifierQueue.Add(new ModifierInformation(type,value));
+            onEnqueueEvent!.Raise();
+            _modifierQueue.Add(new ModifierInformation(type,value));
         }
     }
 }
