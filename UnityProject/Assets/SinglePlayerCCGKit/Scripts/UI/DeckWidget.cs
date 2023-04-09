8acc8572da4cdff14562e5601a76da6b8cdb818b
Eduard Beukes <edpbeukes@gmail.com>
2023-04-09 07:31:49 +0200

    Fixed an issue when clicking on the deck icon multiple times causing multiple loadings of the deck list.

--- UnityProject/Assets/SinglePlayerCCGKit/Scripts/UI/DeckWidget.cs
+++ UnityProject/Assets/SinglePlayerCCGKit/Scripts/UI/DeckWidget.cs
@@ -2,8 +2,10 @@
 // This code can only be used under the standard Unity Asset Store End User License Agreement,
 // a copy of which is available at http://unity3d.com/company/legal/as_terms.
 
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace CCGKit
 {
@@ -17,12 +19,23 @@ namespace CCGKit
         private TextMeshProUGUI textLabel;
 #pragma warning restore 649
 
+        [SerializeField,Tooltip("The button that allows the player to open the deck.")]
+        private Button deckButton;
+
+        [SerializeField, Tooltip("The card pile canvas.")]
+        private Canvas cardPile;
+        
         private int deckSize;
 
         public void SetAmount(int amount)
         {
             deckSize = amount;
             textLabel.text = amount.ToString();
+        }
+
+        public void Update()
+        {
+            deckButton.enabled = !cardPile.gameObject.activeSelf;
         }
 
         public void RemoveCard()
